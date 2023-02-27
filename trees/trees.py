52e2d86342adab0758dca38070b3138e5a102bba
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-02-27 21:54:54 +0300

    add successor and predecessor operations

--- trees/trees.py
+++ trees/trees.py
@@ -2,32 +2,59 @@ from typing import List
 from random import randint
 
 
-class Node:
+class BSTNode:
     def __init__(self, value: int) -> None:
         self.value = value
         self.left = None
         self.right = None
+        self.parent = None
 
+    def get_successor(self) -> 'BSTNode':
+        if self.right:
+            node = self.right
+            while node.left:
+                node = node.left
+            return node
+        else:
+            node = self
+            while node.parent and node == node.parent.right:
+                node = node.parent
+            return node.parent
+
+    def get_predecessor(self) -> 'BSTNode':
+        if self.left:
+            node = self.left
+            while node.right:
+                node = node.right
+            return node
+        else:
+            node = self
+            while node.parent and node == node.parent.left:
+                node = node.parent
+            return node.parent
+            
 
 class BinarySearchTree:
     def __init__(self) -> None:
         self.root = None
 
     def add(self, value: int):
-        def add(tree_node: Node, value: int) -> None:
+        def add(tree_node: BSTNode, value: int) -> None:
             if value < tree_node.value:
                 if tree_node.left is None:
-                    tree_node.left = Node(value)
+                    tree_node.left = BSTNode(value)
+                    tree_node.left.parent = tree_node
                 else:
                     add(tree_node.left, value)
             else:
                 if tree_node.right is None:
-                    tree_node.right = Node(value)
+                    tree_node.right = BSTNode(value)
+                    tree_node.right.parent = tree_node
                 else:
                     add(tree_node.right, value)
 
         if self.root is None:
-            self.root = Node(value)
+            self.root = BSTNode(value)
         else:
             add(self.root, value)
 
@@ -37,7 +64,7 @@ class BinarySearchTree:
     def inorder_traversal(self) -> List[int]:
         values = []
 
-        def inorder_traversal(node: Node):
+        def inorder_traversal(node: BSTNode):
             if node is None:
                 return
             inorder_traversal(node.left)
@@ -58,6 +85,9 @@ def main():
 
     print('Original values:', values)
     print('In order traversal:', tree.inorder_traversal())
+    
+    print('Predecessor of root:', tree.root.get_predecessor().value)
+    print('Successor of root:', tree.root.get_successor().value)
 
 
 if __name__ == '__main__':
